d58983b40c180a201e13f3083444dd229454e4ea
matthewstclaire <matthew.crnich@gmail.com>
2021-03-03 12:52:46 -0700

    commit tree 94342343bc3be3966652f364be583ebf5cceb034

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,13 @@
 // Code DigitalClicker Component Here
+import React from "react";
+
+export default class DigitalClicker extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+  render() {
+    return <div></div>;
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,27 @@
 // Code YouTubeDebugger Component Here
+import React from "react";
+
+export default class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      timesClicked: 0,
+    };
+  }
+
+  handleClick = () => {
+    let newCount = this.state.timesClicked + 1;
+    this.setState({
+      timesClicked: newCount,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>${this.state.timesClicked}</button>
+      </div>
+    );
+  }
+}
